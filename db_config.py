7b25dcd9076012d185564389321e6dfbeecfbd7e
LuisSigala <a6519150031@utch.edu.mx>
2021-06-03 12:10:30 -0600

    all :)

--- db_config.py
+++ db_config.py
@@ -1,12 +1,13 @@
-""" FLask configuration to the database """
+""" FLask configuration to connect to the database """
 from flask_pymongo import pymongo
 import os
 
-DB_USER = os.getenev("DB_USER")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
 
 client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.du4lj.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 
-db = client.test
+# Down here you need to add the database name
+db = client.Badges
---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask_pymongo import pymongo
+import db_config as database
+
+#Resources
+from res.badge import Badge
+from res.badges import Badges
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/all/adults/')
+def get_adults():
+    response = list(database.db.Badges.find({'age': {"$gte": "21"}}))
+
+    for doc in response:
+        doc["_id"] = str(doc['_id'])
+
+    return jsonify(response)
+
+
+@app.route('/all/projection/')
+def get_name_and_age():
+    response = list(database.db.Badges.find({'age': {"$gte": "21"}}, {'name':1, 'age':1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+
+@app.route('/all/kids/')
+def get_kids():
+    response = list(database.db.Badges.find({'age': {"$lte": "12"}}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+@app.route('/all/badges_name/')
+def get_all_badges_name():
+    response = list(database.db.Badges.find({}, {'name': 1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+api.add_resource(Badge, '/new/', '/<string:by>:<string:data>/')
+api.add_resource(Badges, '/all/', '/delete/all/')
+
+
+if __name__ == '__main__':
+    app.run(load_dotenv = True)
